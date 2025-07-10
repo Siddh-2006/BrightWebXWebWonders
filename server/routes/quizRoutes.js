bb27ea8ff6fe83c3eec1bf1c246dacadf78c3241
siddh <shahsiddhp@gmail.com>
2025-07-10 15:50:59 +0530

    Delete server directory

--- server/routes/quizRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const quizController = require('../controllers/quizController');
-
-router.post('/generate', quizController.generateQuizForUser);
-
-router.get('/history/:userId', quizController.getUserQuizHistory);
-
-module.exports = router;
