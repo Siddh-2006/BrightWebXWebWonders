097bfbd549f27cb66c459aef90f01b1c15d7af34
Freny Chauhan <chauhanfreny01@gmail.com>
2025-07-13 22:38:15 +0530

    club code issues are resolved

--- SportsHub/server/middlewares/clubMiddleware.js
+++ SportsHub/server/middlewares/clubMiddleware.js
@@ -10,7 +10,7 @@ const protect = async (req, res, next) => {
   if (!token) return res.status(401).json({ msg: 'No token provided' });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.user = await User.findById(decoded.id).select('-password');
     next();
   } catch (err) {
