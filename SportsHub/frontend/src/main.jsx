51fb3420952933662b4f5d2a4d61716f11321c34
bhaskar <bhscodz7@gmail.com>
2025-08-02 21:55:51 +0530

    fix import error

--- SportsHub/frontend/src/main.jsx
+++ SportsHub/frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import LoginContextProvider from './context/loginContextProvider.jsx';
+import LoginContextProvider from './context/loginContextProvider';
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
